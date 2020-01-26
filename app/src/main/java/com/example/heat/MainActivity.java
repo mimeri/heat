340d34d59bc68a0a0139d104a70e31b5bb3a05c2
Mario Imeri <imerimario@gmail.com>
2020-01-25 23:54:06 -0800

    implement Leo button

--- app/src/main/java/com/example/heat/MainActivity.java
+++ app/src/main/java/com/example/heat/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.heat;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button leoButton;
+
+    TextView helloWorld;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        leoButton = findViewById(R.id.leo);
+
+        helloWorld = findViewById(R.id.textView);
+
+        leoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                helloWorld.setVisibility(View.VISIBLE);
+            }
+        });
     }
+
+
+
+
 }
